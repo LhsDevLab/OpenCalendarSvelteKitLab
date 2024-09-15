bf7d1b2adef00e31c1330f7a3cb3187b9a1ab70b
Lee Han Seong <jkhk7788@gmail.com>
2024-09-15 21:05:29 +0900

    update window

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
   plugins: [sveltekit()],
   define: {
-    __PROJECT_PATH__: `"${process.cwd()}/"`,
+    __PROJECT_PATH__: JSON.stringify(path.resolve(process.cwd()) + path.sep),
   },
 });
